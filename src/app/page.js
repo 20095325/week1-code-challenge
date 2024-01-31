73cecff3f547661a7c8846910a77bfb659d13363
MightyMuso <proslmeyr@icloud.com>
2024-01-31 11:35:03 +0800

    Added logo

--- src/app/page.js
+++ src/app/page.js
@@ -5,11 +5,10 @@ export default function Home() {
     <main className="flex min-h-screen flex-col items-center justify-between p-24">
       <div className="z-10 max-w-5xl w-full items-center justify-between font-mono text-sm lg:flex">
         <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:h-auto lg:w-auto lg:bg-none">
-            By{" "}
             <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
+              src="/logo.jpg"
+              alt="Logo"
+              className=""
               width={100}
               height={24}
               priority
